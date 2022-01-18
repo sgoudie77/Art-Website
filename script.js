1ba1864ac25a82122e5864ca0b8490554e891874
Sean Goudie <sgoudie77@hotmail.com>
2022-01-19 03:12:11 +0700

    toggle and loader updated

--- script.js
+++ script.js
@@ -6,12 +6,10 @@
 
 // Menu Toggle for Responsive Design
 function toggleMenu(){
-    const menuToggle = document.querySelector('.toggle');
-    const navigation = document.querySelector('.navigation');
-    const title = document.querySelector('.hero-title');
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-    title.classList.toggle('active');
+  const menuToggle = document.querySelector('.toggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
 }
 
 // Swiper JS Carousel
